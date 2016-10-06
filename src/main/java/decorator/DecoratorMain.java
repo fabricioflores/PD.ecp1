2a39b4f74a8b3904755a31a1324f3c8fd12720c1
Fabricio Flores <fabrifloresg@gmail.com>
2016-10-06 13:38:28 +0200

    Add missing file in decorator

--- src/main/java/decorator/DecoratorMain.java
+++ src/main/java/decorator/DecoratorMain.java
@@ -6,8 +6,8 @@ public class DecoratorMain {
 		Carro c = new Carro();
 		c.setValor(10000);
 		c.setDescripcion("Alfa Romeo");
-		Extras e1 = new ExtrasMp3(c);
-		Extras e2 = new ExtrasAros(e1);
+		Componente e1 = new ExtrasMp3(c);
+		Componente e2 = new ExtrasAros(e1);
 		System.out.print(e2.getDescripcion());
 		System.out.print("\tTotal: " +  + e2.getValor());
 	}
