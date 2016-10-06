20c502db1733c21675abb5003c1e011b41b93e36
Fabricio Flores <fabrifloresg@gmail.com>
2016-10-06 13:37:25 +0200

    Add files for visitor

---
+++ src/main/java/visitor/Figure.java
@@ -0,0 +1,11 @@
+package visitor;
+
+public abstract class Figure {
+
+    public Figure(String description) {
+    }
+    
+    public abstract double area();
+    public abstract double numberOfSides();
+
+}
