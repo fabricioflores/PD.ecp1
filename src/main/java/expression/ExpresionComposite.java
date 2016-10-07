c5c7130e25df8075fe87c388bbd19e1c70ee5787
Fabricio Flores <fabrifloresg@gmail.com>
2016-10-07 13:26:48 +0200

    Expression: use private instead of protected attributes. Add getters and setters

--- src/main/java/expression/ExpresionComposite.java
+++ src/main/java/expression/ExpresionComposite.java
@@ -1,9 +1,9 @@
 package expression;
 
 public abstract class ExpresionComposite extends Expresion{
-	protected Expresion exp1;
-	protected Expresion exp2;
-	protected String operacion;
+	private Expresion exp1;
+	private Expresion exp2;
+	private String operacion;
 	
 	public ExpresionComposite(Expresion exp1, Expresion exp2){
 		this.exp1 = exp1;
@@ -12,6 +12,30 @@ public abstract class ExpresionComposite extends Expresion{
 	
 	public String toString(){
 		return "(" + exp1.toString() + this.operacion + exp2.toString() + ")";
+	}
+
+	public Expresion getExp1() {
+		return exp1;
+	}
+
+	public void setExp1(Expresion exp1) {
+		this.exp1 = exp1;
+	}
+
+	public Expresion getExp2() {
+		return exp2;
+	}
+
+	public void setExp2(Expresion exp2) {
+		this.exp2 = exp2;
+	}
+
+	public String getOperacion() {
+		return operacion;
+	}
+
+	public void setOperacion(String operacion) {
+		this.operacion = operacion;
 	}
 
 }
--- src/main/java/expression/Multiplicacion.java
+++ src/main/java/expression/Multiplicacion.java
@@ -4,12 +4,12 @@ public class Multiplicacion extends ExpresionComposite{
 
 	public Multiplicacion(Expresion exp1, Expresion exp2) {
 		super(exp1, exp2);
-		this.operacion = "*";
+		this.setOperacion("*");
 	}
 
 	@Override
 	public int resolve() {
-		return this.exp1.resolve() * this.exp2.resolve();
+		return this.getExp1().resolve() * this.getExp2().resolve();
 	}
 
 }
